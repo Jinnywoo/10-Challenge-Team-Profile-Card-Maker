311880025e7cc2d5b846fd9f2d1131a5d9773a11
Ares <jinwoo2639@gmail.com>
2022-11-11 20:19:24 -0800

    maybe cookie cuttering the rest of the roles isn't that much more efficient

--- index.js
+++ index.js
@@ -39,3 +39,65 @@ function promptManager(){
             choicePrompts();
 })
 }
+function promptEngineer(){
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: `What is the Engineer's name?`,
+                name: 'engineerName',
+            },
+            {
+                type: 'input',
+                message: `What is the Engineer's id number?`,
+                name: 'engineerId',
+            },
+            {
+                type: 'input',
+                message: `What is the Engineers's email?`,
+                name: 'engineerEmail',
+            },
+            {
+                type: 'input',
+                message: `What is the Engineers's github?`,
+                name: 'engineerGithub',
+            },
+        ])
+        .then((response) => {
+            var engineer = new Engineer(response.engineerName, response.engineerId, response.engineerEmail, response.engineerGithub);
+            employees.push(engineer);
+
+            choicePrompts();
+})
+}
+function promptManager(){
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: `What is the Team Manager's name?`,
+                name: 'managerName',
+            },
+            {
+                type: 'input',
+                message: `What is the Team Manager's id number?`,
+                name: 'managerId',
+            },
+            {
+                type: 'input',
+                message: `What is the Team Manager's email?`,
+                name: 'managerEmail',
+            },
+            {
+                type: 'input',
+                message: `What is the Team Manager's office number?`,
+                name: 'managerOfficenumber',
+            },
+        ])
+        .then((response) => {
+            var manager = new Manager(response.managerName, response.managerId, response.managerEmail, response.managerOfficenumber);
+            employees.push(manager);
+
+            choicePrompts();
+})
+}
